cc300d438a76aa5e5cc92edbb389882b702516b7
Dilip <dilipk1261@gmail.com>
2022-09-24 17:26:04 +0530

    Implementing Page Scrolling,Event Delegation

--- script.js
+++ script.js
@@ -35,17 +35,51 @@ btnscroll.addEventListener("click",function(e){
 })
 
 //Types of events and event handlers
-const h=document.querySelector("h1")
-// h.addEventListener('mouseenter',function(){
-//     alert("U r reading message");
-// })
+// const h=document.querySelector("h1")
+// // h.addEventListener('mouseenter',function(){
+// //     alert("U r reading message");
+// // })
 
-// By using addEventListener we can use mulitple event on one element and
-//  we can also delete the eventlistener.
-const alerth1=function(){
-    alert("U r reading")
+// // By using addEventListener we can use mulitple event on one element and
+// //  we can also delete the eventlistener.
+// const alerth1=function(){
+//     alert("U r reading")
 
-    h.removeEventListener('mouseenter',alerth1);
-}
-h.addEventListener('mouseenter',alerth1);   //we can delete the eventhandler in this way
+//     h.removeEventListener('mouseenter',alerth1);
+// }
+// h.addEventListener('mouseenter',alerth1);   //we can delete the eventhandler in this way
 
+// Event propagation
+document.querySelectorAll(".navlist-link").forEach(element => {
+    
+    element.addEventListener("click",
+    function(e){
+        this.style.backgroundColor="lightgreen"
+    }
+)});
+document.querySelector(".navbar").addEventListener("click",
+    function(e){
+        this.style.backgroundColor="lightblue"
+    }
+);
+
+// page scrolling
+// document.querySelectorAll(".navlist-link").forEach(function(ele){
+//     ele.addEventListener("click",function(e){
+//         e.preventDefault();
+//         const id=this.getAttribute('href')
+//         console.log(id)
+//         // console.log(document.querySelector(id))
+//         document.querySelector(id).scrollIntoView({behavior:"smooth"})
+//     })
+// });
+
+//Event delegation 
+document.querySelector(".navlist").addEventListener("click",function(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('navlist-link')){
+        const id=e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior:"smooth"});
+    }
+});
